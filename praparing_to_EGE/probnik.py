7141b40c9a3c7647334d226596838bd5690af42f
kira <zhulin@1060.ru>
2023-02-08 15:15:03 +0300

    Commit

--- praparing_to_EGE/probnik.py
+++ praparing_to_EGE/probnik.py
@@ -581,5 +581,31 @@ def Type_23_5977():
     print(find_paths(33))
 
 
+def Type_25_27422():
+    flag = True
+    flag_n = True
+    for number in range(174457, 174506):
+        for i in range(2, number):
+            flag_n = True
+            for n in range(2, i):
+                if i % n == 0:
+                    flag_n = False
+                    break
+                if not flag_n:
+                    break
+            else:
+                for j in range(2, number):
+                    flag_n = True
+                    for n in range(2, i):
+                        if j % n == 0:
+                            flag_n = False
+                            break
+                        if not flag_n:
+                            break
+                    else:
+                        if number % j == 0 and number % i == 0 and j != i:
+                            print(i, j)
+
+
 if __name__ == '__main__':
-    Type_23_5977()
+    Type_25_27422()
